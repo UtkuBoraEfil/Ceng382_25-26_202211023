9fc395c5c87d57ffa8802d4ec9ff288519c6da35
UtkuBoraEfil <utqbora@hotmail.com>
2025-04-23 14:43:32 +0300

    update

--- RazorProject/obj/Debug/net9.0/RazorProject.AssemblyInfo.cs
+++ RazorProject/obj/Debug/net9.0/RazorProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82a542bc4256a1479124d82fd5d44e8fd48fb933")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bb2744c10d61ef898691c635e1b04c0738c8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
